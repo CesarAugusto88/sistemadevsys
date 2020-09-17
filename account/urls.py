6183cdc4bee1409bf8effcf086904096ec0563d9
Cesar Costa <cesarcosta.augustos@gmail.com>
2020-09-17 13:52:58 -0300

    Modificado funções da cadastro de usuários

--- account/urls.py
+++ account/urls.py
@@ -6,12 +6,11 @@ app_name = 'account'
 urlpatterns = [
     #path('registrar/', views.register.as_view(), name='registrar'),
     # Registra USUARIO cliente/funcionario
-    path('alterar_senha_cliente/', views.alterar_senha_cli, name='alterar_senha_cli'),
-    path('alterar_senha_funcionario/', views.alterar_senha_fun, name='alterar_senha_fun'),
-    path('registrar/clie', views.register_clie, name='registrar_cliente'),
-    path('registrar/func', views.register_func, name='registrar_funcionario'),
-    path('cadastro/cliente', views.register_cliente, name='cad_cliente'),
-    path('funcionario', views.register_funcionario, name='cad_funcionario'),
-    path('cadastro/submit', views.submit_register_cliente),
-    path('submit', views.submit_register_funcionario),
+    path('registrar/cliente', views.register_cliente, name='registrar_cliente'),
+    path('cadastro/cadastrar-cliente/', views.cadastrar_cliente, name="cadastrar_cliente"),
+    path('registrar/funcionario', views.register_funcionario, name='registrar_funcionario'),
+    path('cadastro/cadastrar-funcionario/', views.cadastrar_funcionario, name="cadastrar_funcionario"),
+    path('alterar_senha_cliente/', views.alterar_senha_cliente, name='alterar_senha_cliente'),
+    path('alterar_senha_funcionario/', views.alterar_senha_funcionario, name='alterar_senha_funcionario'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -9,6 +9,8 @@ from django.views.generic import CreateView, ListView, TemplateView
 
 from core.models import Cliente, Funcionario
 
+from core.forms import FuncionarioForm, ClienteForm
+
 
 """ class register(CreateView):
 
@@ -18,110 +20,59 @@ from core.models import Cliente, Funcionario
 
 # --------Registro Cliente-------------
 @login_required(login_url="/login/")
-def register_clie(request):
+def register_cliente(request):
     if request.method == "POST":
         form_usuario = UserCreationForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
-            # depois que cadastrar redireciona para cadastro de cliente
-            return redirect('account:cad_cliente')
+            # depois que criar user redireciona para cadastro de cliente
+            return redirect('account:cadastrar_cliente')
     else:
         form_usuario = UserCreationForm()
-    return render(request, 'register_clie.html', {'form_usuario': form_usuario})
+    return render(request, 'register_cliente.html', {'form_usuario': form_usuario})
 
-# mostra registro cliente
+#Cadastrar Cliente
 @login_required(login_url="/login/")
-def register_cliente(request):
-    id_cliente = request.GET.get("id")
-    usuario = User.objects.all().order_by('-username')
-    dados = {}
-    if id_cliente:
-        dados["cliente"] = Cliente.objects.get(id=id_cliente)
-    return render(request, 'cadastrar_cliente.html', {'usuario': usuario}, dados)
-
-# submit registro cliente
-# colocar foreinkey usando id
-@login_required(login_url="/login/")
-def submit_register_cliente(request):
-    # verificar com ID de usuario para restringir...
-    # usuario = 
+def cadastrar_cliente(request):
+    """ Cria formulário cliente."""
     if request.method == "POST":
-        nome = request.POST.get("nome")
-        razao_social = request.POST.get("razao_social")
-        tipo_pessoa = request.POST.get("tipo_pessoa")
-        cpf_cnpj= request.POST.get("cpf_cnpj")
-        rg_ie = request.POST.get("rg_ie")
-        endereco = request.POST.get("endereco")
-        cidade = request.POST.get("cidade")
-        cep = request.POST.get("cep")
-        uf = request.POST.get("uf")
-        email = request.POST.get("email")
-        fone1 = request.POST.get("fone1")
-        usuario_cli_id = request.POST.get("usuario_cli_id")
-        Cliente.objects.create(
-            nome=nome,
-            razao_social=razao_social,
-            tipo_pessoa=tipo_pessoa,
-            cpf_cnpj=cpf_cnpj,
-            rg_ie=rg_ie,
-            endereco=endereco,
-            cidade=cidade,
-            cep=cep,
-            uf=uf,
-            email=email,
-            fone1=fone1,
-            usuario_cli_id=usuario_cli_id,
-        )
-    return redirect("/devsys/funcionario")
-
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            novo = Cliente(**form.cleaned_data)
+            novo.save()
+            return redirect('funcionario')
+    else:
+        form = ClienteForm()
+    return render(request, "cadastrar_cliente.html", {"form": form})
 
 # --------Registro Funcionário-------------
 @login_required(login_url="/login/")
-def register_func(request):
+def register_funcionario(request):
     if request.method == "POST":
         form_usuario = UserCreationForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
-            return redirect('account:cad_funcionario')
+            return redirect('account:cadastrar_funcionario')
     else:
         form_usuario = UserCreationForm()
-    return render(request, 'register_func.html', {'form_usuario': form_usuario})
-
-# mostra registro funcionario
-@login_required(login_url="/login/")
-def register_funcionario(request):
-    id_funcionario = request.GET.get("id")
-    # ordena pelo ultimo, no caso o ID do ultimo cadastrado
-    usuario = User.objects.all().order_by('-username')
-    dados = {}
-    if id_funcionario:
-        dados["funcionario"] = Funcionario.objects.get(id=id_funcionario)
-    return render(request, 'cadastrar_funcionario.html', {'usuario': usuario}, dados)
-
+    return render(request, 'register_funcionario.html', {'form_usuario': form_usuario})
 
 @login_required(login_url="/login/")
-def submit_register_funcionario(request):
-    if request.POST:
-        nome = request.POST.get("nome")
-        fone1 = request.POST.get("fone1")
-        endereco = request.POST.get("endereco")
-        cidade = request.POST.get("cidade")
-        cep = request.POST.get("cep")
-        uf = request.POST.get("uf")
-        usuario_fun_id = request.POST.get("usuario_fun_id")
-        Funcionario.objects.create(
-            nome=nome,
-            fone1=fone1,
-            endereco=endereco,
-            cidade=cidade,
-            cep=cep,
-            uf=uf,
-            usuario_fun_id=usuario_fun_id
-        )
-    return redirect("/devsys/funcionario")
+def cadastrar_funcionario(request):
+    """ Cria formulário funcionário."""
+    if request.method == "POST":
+        form = FuncionarioForm(request.POST)
+        if form.is_valid():
+            novo = Funcionario(**form.cleaned_data)
+            novo.save()
+            return redirect('funcionario')
+    else:
+        form = FuncionarioForm()
+    return render(request, "cadastrar_funcionario.html", {"form": form})
 
+# Alterar Senhas
 @login_required(login_url='/login/')
-def alterar_senha_cli(request):
+def alterar_senha_cliente(request):
     if request.method == "POST":
         form_senha = PasswordChangeForm(request.user, request.POST)
         if form_senha.is_valid():
@@ -130,10 +81,10 @@ def alterar_senha_cli(request):
             return redirect('/login/')
     else:
         form_senha = PasswordChangeForm(request.user)
-    return render(request, 'alterar_senha_cli.html', {'form_senha': form_senha})
+    return render(request, 'alterar_senha_cliente.html', {'form_senha': form_senha})
 
 @login_required(login_url='/login/')
-def alterar_senha_fun(request):
+def alterar_senha_funcionario(request):
     if request.method == "POST":
         form_senha = PasswordChangeForm(request.user, request.POST)
         if form_senha.is_valid():
@@ -142,4 +93,4 @@ def alterar_senha_fun(request):
             return redirect('/login/')
     else:
         form_senha = PasswordChangeForm(request.user)
-    return render(request, 'alterar_senha_fun.html', {'form_senha': form_senha})
+    return render(request, 'alterar_senha_funcionario.html', {'form_senha': form_senha})
